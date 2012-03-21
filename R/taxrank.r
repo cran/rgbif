737892841a6e447f580854081f8875bde554c596
Scott Chamberlain <myrmecocystus@gmail.com>
2012-03-21 00:00:00 +0000

    version 0.0.1

---
+++ R/taxrank.r
@@ -0,0 +1,12 @@
+#' Get values to be used for (taxonomic) rank arguments in GBIF API methods. 
+#' @export
+#' @examples \dontrun{
+#' taxrank()
+#' }
+taxrank <- function()
+{
+  c(
+    'kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species', 
+    'infraspecific'
+    )
+}
